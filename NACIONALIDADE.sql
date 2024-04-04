53e6425cd329c3c6a4fed7f9a910a2f31b486f1b
geovanapro <145471347+geovanapro@users.noreply.github.com>
2024-04-04 15:44:48 -0300

    NACIONALIDADE.sql

---
+++ NACIONALIDADE.sql
@@ -0,0 +1,12 @@
+  Para adicionar o campo 'Americana' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Americana' WHERE ID IN (1, 3, 4, 6, 7, 8, 10, 11, 12, 14, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27)
+  Para adicionar o campo 'Britânica' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Britânica' WHERE ID IN (2, 5) 
+  Para adicionar o campo 'Espanhola' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Espanhola' WHERE ID IN (9, 13) 
+  Para adicionar o campo 'Canadense' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Canadense' WHERE ID IN (15)  
+  Para adicionar o campo 'Brasileira' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Brasileira' WHERE ID IN (22, 27, 28, 29)  
+  Para adicionar o campo 'Nova Zelândia' em NACIONALIDADE:
+UPDATE ENTRETERIMENTO SET NACIONALIDADE = 'Nova Zelândia' WHERE ID IN (20)  
